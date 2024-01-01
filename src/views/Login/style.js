44438b261351b45ef076083ec687ba95182652b8
Luis Galea <luisgalea31@gmail.com>
2024-01-01 12:47:47 -0400

    feat<src\views\login\style.js>: add news styles

--- src/views/Login/style.js
+++ src/views/Login/style.js
@@ -10,11 +10,50 @@ export const styles = StyleSheet.create({
         // backgroundColor: 'red',
         justifyContent: 'space-between',
         flexDirection: 'row',
-        width: '100%',
+        width: '100%'
+    },
+    ctn_illustration: {
+        height: '13%',
+    },
+    ctn_img:{
+        position: 'absolute',
+        zIndex: 1,
+        height: 200,
+        width: 200,
+        right: '13%',
+        top: '-150%',
     },
     ctn_login_info: {
         height: '100%',
         borderRadius: 50,
         bottom: '27%'
-    }
+    },
+    ctn_UI: {
+        height: '55%'
+    },
+    ctn_circles: {
+        height: '45%',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        width: '100%'
+    },
+    ellipseContainerOrange: {
+        width: 200,
+        height: 200,
+        top: '15%',
+        left: '-30%',
+        borderRadius: 100,      
+        // opacity: 0.58,
+    },
+    ellipseContainerPurpple: {
+        width: 200,
+        height: 200,
+        top: '15%',
+        right: '-30%',
+        borderRadius: 100,      
+        // opacity: 0.58,
+    },
+    absolute: {
+        position: 'absolute'
+    },
 });
