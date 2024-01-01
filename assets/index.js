324ecc0091ef7009a4390b91179c320f4fcf07d2
Luis Galea <luisgalea31@gmail.com>
2024-01-01 12:50:12 -0400

    feat<assets\index.js>: add new routes

--- assets/index.js
+++ assets/index.js
@@ -5,5 +5,7 @@ import figure3 from './figure_3.png';
 import figure4 from './figure_4.png';
 import figure5 from './figure_5.png';
 import figure6 from './figure_6.png';
+import circlePurpple from './circle_purpple.png'
+import illustration from './illustration.png'
 
-export { figure1, figure2, figure3, figure4, figure5, figure6 };
+export { figure1, figure2, figure3, figure4, figure5, figure6, circlePurpple, illustration };
